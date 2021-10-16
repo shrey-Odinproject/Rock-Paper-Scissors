6180c06f7000181682935d1758dd74967d94b1f7
shrey-Odinproject <sonishreyf1@gmail.com>
2021-10-16 23:20:52 +0530

    finally added user input and created a game fr 5 rounds displaying score every round and then displaying final score

--- RPS.js
+++ RPS.js
@@ -9,11 +9,11 @@ function singleRound(playerselection, computerselection) {
     playerselection = playerselection.slice(0, 1).toUpperCase() + playerselection.slice(1).toLowerCase();
 
     if (playerselection === 'Scissors' && computerselection === 'Paper') {
-        console.log(`you win ! ${playerselection} beats ${computerselection}`)
+        return (`you win ! ${playerselection} beats ${computerselection}`)
     }
 
     else if (playerselection === 'Rock' && computerselection === 'Scissors') {
-        console.log(`you win ! ${playerselection} beats ${computerselection}`)
+        return (`you win ! ${playerselection} beats ${computerselection}`)
     }
 
     else if (playerselection === 'Paper' && computerselection === 'Rock') {
@@ -21,7 +21,7 @@ function singleRound(playerselection, computerselection) {
     }
 
     else if (playerselection === computerselection) {
-        return (`DRAW!!! both picked ${playerselection}`)
+        return (`draw!!! both picked ${playerselection}`)
     }
 
     else {
@@ -32,18 +32,41 @@ function singleRound(playerselection, computerselection) {
 
 
 
+
+
 function game() {
-    let playerScore = 0, computerScore = 0
-    
-    for (i = 0; i < 1; i++) {
-        
-        let roundResult = String(console.log(`round num ${i+1} RESULT: `, singleRound(prompt('entr ur choice'), computerPlay())))
-        if (roundResult.includes('win')){
-            console.log('yeeeeeeee')
+    player = 0
+    comp = 0
+    for (i = 0; i < 5; i++) {
+        reoundResult = singleRound(prompt('entr ur choice'), computerPlay())
+        console.log(`result of round ${i+1} is ${reoundResult}`)
+
+        if (reoundResult.includes('win')) {
+            player++
         }
+        else if (reoundResult.includes('lost')) {
+            comp++
+        }
+        else {
+            // do nothing for draw
+        }
+
     }
-    /*console.log(playerScore,computerScore)
-    let final = (playerScore >= computerScore) ? 'player won' : 'comp won'
-    return final*/
+    if (player>comp){
+        let finalScore=`player won with score player ${player} : computer ${comp}`
+        return finalScore
+    }
+    else if (comp>player){
+        let finalScore=`comp won with score player ${player} : computer ${comp}`
+        return finalScore
+    }
+    else{
+        let finalScore=`its a DRAW with score player ${player} : computer ${comp}`
+        return finalScore
+    }
+    
+    
+
 }
+
 console.log(game())
