99e5393c335961b1ebc2eefea4a82eb019f04d55
shrey-Odinproject <sonishreyf1@gmail.com>
2021-10-16 17:50:27 +0530

    created the singleRound() that runs simulation of RPS once and returns appropriate string. Switch statement is messy to use so i used if-else, also user input is made case-insensitive.

--- RPS.js
+++ RPS.js
@@ -1,7 +1,33 @@
-function computerPlay(){
-    let choices=['Rock','Paper','Scissors'];
-    let choice=choices[Math.floor(Math.random() * choices.length)];
+function computerPlay() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choice = choices[Math.floor(Math.random() * choices.length)];
     //console.log('comp choice is : '+ choice)
     return choice
 }
-computerPlay()
+
+function singleRound(playerselection, computerselection) {
+    playerselection = playerselection.slice(0, 1).toUpperCase() + playerselection.slice(1).toLowerCase();
+    
+    if (playerselection === 'Scissors' && computerselection === 'Paper') {
+        console.log(`you win ! ${playerselection} beats ${computerselection}`);
+    }
+
+    else if (playerselection === 'Rock' && computerselection === 'Scissors') {
+        console.log(`you win ! ${playerselection} beats ${computerselection}`);
+    }
+
+    else if (playerselection === 'Paper' && computerselection === 'Rock') {
+        return(`you win ! ${playerselection} beats ${computerselection}`);
+    }
+
+    else if (playerselection === computerselection) {
+        return(`DRAW!!! both picked ${playerselection}`)
+    }
+
+    else {
+        return(`you lost ! ${computerselection} beats ${playerselection}`)
+    }
+
+}
+
+console.log(singleRound(prompt('entr ur choice'), computerPlay()))
