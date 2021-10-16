7e496e57e7968dc28a80dbe86924f996804782d0
shrey-Odinproject <sonishreyf1@gmail.com>
2021-10-16 19:12:49 +0530

    created game() wich runs the single simulation 5 times, unable to keep track of total scores and display final result

--- RPS.js
+++ RPS.js
@@ -7,27 +7,43 @@ function computerPlay() {
 
 function singleRound(playerselection, computerselection) {
     playerselection = playerselection.slice(0, 1).toUpperCase() + playerselection.slice(1).toLowerCase();
-    
+
     if (playerselection === 'Scissors' && computerselection === 'Paper') {
-        console.log(`you win ! ${playerselection} beats ${computerselection}`);
+        console.log(`you win ! ${playerselection} beats ${computerselection}`)
     }
 
     else if (playerselection === 'Rock' && computerselection === 'Scissors') {
-        console.log(`you win ! ${playerselection} beats ${computerselection}`);
+        console.log(`you win ! ${playerselection} beats ${computerselection}`)
     }
 
     else if (playerselection === 'Paper' && computerselection === 'Rock') {
-        return(`you win ! ${playerselection} beats ${computerselection}`);
+        return (`you win ! ${playerselection} beats ${computerselection}`)
     }
 
     else if (playerselection === computerselection) {
-        return(`DRAW!!! both picked ${playerselection}`)
+        return (`DRAW!!! both picked ${playerselection}`)
     }
 
     else {
-        return(`you lost ! ${computerselection} beats ${playerselection}`)
+        return (`you lost ! ${computerselection} beats ${playerselection}`)
     }
 
 }
 
-console.log(singleRound(prompt('entr ur choice'), computerPlay()))
+
+
+function game() {
+    let playerScore = 0, computerScore = 0
+    
+    for (i = 0; i < 1; i++) {
+        
+        let roundResult = String(console.log(`round num ${i+1} RESULT: `, singleRound(prompt('entr ur choice'), computerPlay())))
+        if (roundResult.includes('win')){
+            console.log('yeeeeeeee')
+        }
+    }
+    /*console.log(playerScore,computerScore)
+    let final = (playerScore >= computerScore) ? 'player won' : 'comp won'
+    return final*/
+}
+console.log(game())
