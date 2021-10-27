d0d43442aad775f24abc2b1c2e0735ab6baec521
shrey-Odinproject <sonishreyf1@gmail.com>
2021-10-27 12:02:30 +0530

    added buttons to html and styled it with css, used DOM manipulation to run a single round of RPS whenever we press a button

--- RPS.js
+++ RPS.js
@@ -1,3 +1,12 @@
+// MAKING THE UI PART
+function func(e) {
+    console.log(singleRound(e.target.textContent, computerPlay())) //console.log(e.target.textContent) gives string in the respective button
+}// function that runs when we click
+const buttons = document.querySelectorAll('button');// nodelist of all button
+buttons.forEach(button => button.addEventListener('click', func))// for all buttons we run event siltner click
+
+
+
 function computerPlay() {
     let choices = ['Rock', 'Paper', 'Scissors'];
     let choice = choices[Math.floor(Math.random() * choices.length)];
@@ -29,7 +38,7 @@ function singleRound(playerselection, computerselection) {
     }
 
 }
-console.log(singleRound(prompt('entr ur choice'), computerPlay()))
+//console.log(singleRound(prompt('entr ur choice'), computerPlay()))
 /*
 function game() {
     player = 0
@@ -61,11 +70,12 @@ function game() {
         let finalScore=`its a DRAW with score player ${player} : computer ${comp}`
         return finalScore
     }
-    
-    
+
+
 
 }
 
 console.log(game())
 
 */
+
