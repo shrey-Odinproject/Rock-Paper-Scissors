221ef40aff806520833822ceff22c8f38fce6ccb
shrey-Odinproject <sonishreyf1@gmail.com>
2021-10-28 23:07:26 +0530

    added the play again button which refreshes the page and when anyone reaches 5 points the text contents are set to blank string to stimulate a non responsive page forcing user to click on play again

--- RPS.js
+++ RPS.js
@@ -1,37 +1,10 @@
 // MAKING THE UI PART
 
-const buttons = document.querySelectorAll('button');// nodelist of all button
+const buttons = document.querySelectorAll('.choice');// nodelist of all button
 const body = document.querySelector('body'); // the <body> tag
 
-function playOnPress(e) {
-    results.textContent = (singleRound(e.target.textContent, computerPlay()))
-    if (results.textContent.includes('win')) { ++pl }
-    else if (results.textContent.includes('lost')) { ++cp }
-    else { }
-
-    player.textContent = ` player : ${pl}`
-    comp.textContent = `computer : ${cp}`
-    if (pl === 3) {
-        player.textContent = ''
-        gameOver.textContent = 'YOU WIN'
-        comp.textContent = ''
-    }
-    else if (cp === 3) {
-        player.textContent = ''
-        gameOver.textContent = 'COMP WINS'
-        comp.textContent = ''
-    }
-}// function that runs 1 round of rps when we click
 buttons.forEach(button => button.addEventListener('click', playOnPress))// for all buttons we run event siltner click
-
-function animateHover(e) {
-    e.target.classList.add('playing')
-}
 buttons.forEach(button => button.addEventListener('mouseover', animateHover))
-
-function animateUnhover(e) {
-    e.target.classList.remove('playing')
-}
 buttons.forEach(button => button.addEventListener('mouseout', animateUnhover))
 
 const results = document.createElement('div')
@@ -44,7 +17,39 @@ const comp = document.querySelector('.comp')// aim is to display score in text c
 let pl = 0
 let cp = 0
 
+const refresh=document.querySelector('.refresh button')// play again button refreshes
+refresh.addEventListener('click',(e)=>{window.location.reload()})
+refresh.addEventListener('mouseover',animateHover)
+refresh.addEventListener('mouseout',animateUnhover)
 
+function playOnPress(e) {
+    results.textContent = (singleRound(e.target.textContent, computerPlay()))
+    if (results.textContent.includes('win')) { ++pl }
+    else if (results.textContent.includes('lost')) { ++cp }
+    else { }
+
+    player.textContent = ` player : ${pl}`
+    comp.textContent = `computer : ${cp}`
+    if (pl >= 5) {
+        player.textContent = ''
+        gameOver.textContent = 'YOU WIN'
+        comp.textContent = ''
+        results.textContent=''
+    }
+    else if (cp >= 5) {
+        player.textContent = ''
+        gameOver.textContent = 'COMP WINS'
+        comp.textContent = ''
+        results.textContent=''
+    }
+    
+}// function that runs 1 round of rps when we click button
+function animateHover(e) {
+    e.target.classList.add('playing')
+}
+function animateUnhover(e) {
+    e.target.classList.remove('playing')
+}
 // returns a random string out of the three
 function computerPlay() {
     let choices = ['Rock', 'Paper', 'Scissors'];
@@ -52,7 +57,6 @@ function computerPlay() {
     //console.log('comp choice is : '+ choice)
     return choice
 }
-
 function singleRound(playerselection, computerselection) {
     playerselection = playerselection.slice(0, 1).toUpperCase() + playerselection.slice(1).toLowerCase();
 
@@ -77,6 +81,8 @@ function singleRound(playerselection, computerselection) {
     }
 
 }
+
+
 //console.log(singleRound(prompt('entr ur choice'), computerPlay()))
 /*
 function game() {
